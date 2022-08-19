63b045b29dc2f311af3df1647f7c8328a6673731
jesjo001 <automationlounge@gmail.com>
2022-08-19 12:35:00 +0100

    jet config files updated

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ["**/**/*.test.ts"],
+  testMatch: ['**/**/*.test.ts'],
   verbose: true,
   forceExit: true,
   // clearMocks: true,
   resetMocks: true,
-  restoreMocks: true,
-};
+  restoreMocks: true
+}
